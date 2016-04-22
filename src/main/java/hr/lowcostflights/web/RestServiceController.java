42fec4652d1625ada61eaf8c14dcb8ec0f9d5616
Matko Jun <matko.jun@gmail.com>
2016-04-22 22:12:12 +0200

    Enabled CORS for REST methods.

--- src/main/java/hr/lowcostflights/web/RestServiceController.java
+++ src/main/java/hr/lowcostflights/web/RestServiceController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,8 @@ import hr.lowcostflights.service.SearchService;
  * @author matko
  *
  */
+
+@CrossOrigin
 @RestController
 public class RestServiceController {
 
